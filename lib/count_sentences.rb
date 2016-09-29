25969538032dbf3ec31deb728066436643d62611
nickscaglione <nicholas.a.scaglione@gmail.com>
2016-09-29 15:08:04 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+  #  self.squeeze.split(/\!|\.|\?/).size
+    self.split(/\!|\.|\?/).delete_if { |sentence| sentence == "" }.size
   end
 end
